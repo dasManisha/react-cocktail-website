e097b3e4ac87f08f8e20da5c015e8b671467faa8
Manisha Das <mamonddas@gmail.com>
2022-07-07 09:07:24 -0700

    Initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident cupiditate aspernatur, deleniti, dolorem qui quo rerum quibusdam magni animi ullam harum vel voluptate voluptas minima quidem perspiciatis repellat accusantium consequuntur ipsa aliquid fuga in saepe voluptatem? Repudiandae maiores sapiente voluptas.
+      </p>
+    </section>
+  )
+}
+
+export default About
